45aa069d7eb0ec666f19a508f4b25db8e62f0498
Nikita Makarov <quiz.beat@gmail.com>
2015-01-13 02:00:07 +0300

    delete comment from main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,6 @@
 int main(int argc, char *argv[]) {
 
     QApplication a(argc, argv);
-    /*
-    QGraphicsScene scene;
-    QGraphicsView view(&scene);
-
-    QPixmap image = QPixmap("/Users/air/Documents/Computer Graphics/CGCourseWork/doge.jpeg");
-    image = image.scaledToHeight(image.height() * 0.5f);
-    image = image.scaledToWidth(image.width() * 0.5f);
-
-    QGraphicsPixmapItem item(image);
-    scene.addItem(&item);
-
-    view.show();
-    */
-
     Widget w;
     w.show();
 
