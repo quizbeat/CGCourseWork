539099690fc4588d8e4ff36464991ac356571550
Nikita Makarov <quiz.beat@gmail.com>
2015-01-13 00:45:41 +0300

    switch to bug

--- widget.cpp
+++ widget.cpp
@@ -5,7 +5,7 @@ Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
-    path = "/Users/air/Documents/Computer Graphics/CGCourseWork/doge.jpeg";
+    path = "/Users/air/Documents/Computer Graphics/CGCourseWork/doge4.bmp";
     loadImage(path);
 
     ui->setupUi(this);
@@ -36,6 +36,7 @@ void Widget::medianFiltration() {
     int frameOffsetByX = frameWidth / 2;
     int frameOffsetByY = frameHeight / 2;
 
+    /*
     for (int k = 0; k < intensity; k++) {
 
         for (int x = 0; x < image.width() - frameWidth + 1; x++) {
@@ -44,21 +45,58 @@ void Widget::medianFiltration() {
                 int currentPixel = 0;
                 for (int i = 0; i < frameWidth; i++) {
                     for (int j = 0; j < frameHeight; j++) {
-                        item.color = QColor(image.pixel(QPoint(x + j, y + i)));
-                        item.num = currentPixel;
-                        pixels[currentPixel] = item;
-                        currentPixel++;
+                        pixels[currentPixel++] = QColor(image.pixel(QPoint(x + i, y + j)));
                     }
                 }
 
                 QColor newPixel = getMedian(pixels);
                 image.setPixel(x + frameOffsetByX, y + frameOffsetByY, newPixel.rgb());
-                //image.setPixel(x + frameOffsetByX, y + frameOffsetByY, qRgb(0.0, 0.0, 0.0));
-                update();
             }
         }
 
     }
+    */
+    /*
+    for (int k = 0; k < intensity; k++) {
+
+        for (int x = 0; x < image.width() - 2; x++) {
+            for (int y = 0; y < image.height() - 2; y++) {
+
+                //int currentPixel = 0;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        pixels.push_back(QColor(image.pixel(QPoint(x + i, y + j))));
+                    }
+                }
+
+                QColor newPixel = getMedian(pixels);
+                image.setPixel(x + 1, y + 1, newPixel.rgb());
+                pixels.clear();
+            }
+        }
+
+    }
+    */
+
+    for (int i = 0; i < intensity; i++) {
+
+            for (int x = 1; x < image.width(); x++) {
+                for (int y = 1; y < image.height(); y++) {
+
+                    int pos = 0;
+                    for (int i = 0; i < 3; i++)
+                        for (int j = 0; j < 3; j++) {
+                             pixels[pos].color = QColor(image.pixel(QPoint(x + i, y + j)));
+                             pixels[pos].num = pos;
+                             pos++;
+                        }
+                    QColor avg = getMedian(pixels);
+                    image.setPixel(x, y, avg.rgb());
+                }
+            }
+
+        }
+
 }
 
 qreal Widget::getLightness(QColor pixel) {
@@ -66,31 +104,60 @@ qreal Widget::getLightness(QColor pixel) {
 }
 
 QColor Widget::getMedian(vector<mQColor> &pixels) {
+    /*
+    QColor median;
+
+    int amount = pixels.size();
+    int middle = amount / 2 + 1;
+
+    int medianR = 0;
+    int medianG = 0;
+    int medianB = 0;
+
+    vector<int> pixelsR(amount);
+    for (int i = 0; i < amount; i++) {
+        pixelsR[i] = pixels[i].red();
+    }
+    sort(pixelsR.begin(), pixelsR.end());
+    medianR = pixelsR[middle];
+
+    vector<int> pixelsG(amount);
+    for (int i = 0; i < amount; i++) {
+        pixelsG[i] = pixels[i].green();
+    }
+    sort(pixelsG.begin(), pixelsG.end());
+    medianG = pixelsG[middle];
+
+    vector<int> pixelsB(amount);
+    for (int i = 0; i < amount; i++) {
+        pixelsB[i] = pixels[i].blue();
+    }
+    sort(pixelsB.begin(), pixelsB.end());
+    medianB = pixelsB[middle];
+
+    median = qRgb(medianG, medianR, medianB);
+
+    return median;
+    */
 
     int amount = pixels.size();
     QColor median;
-
     vector<mLightness> lights(amount);
 
-    for (int i = 0; i < amount; i++) {
-        lights[i].lightness = pixels[i].color.lightnessF();
+    for (int i = 0; i < 9; i++) {
+        lights[i].lightness = pixels[i].color.lightness();
         lights[i].num = pixels[i].num;
     }
 
     sort(lights.begin(), lights.end(), lightnessComparator);
 
-    int middle = amount / 2 + 1;
+    int middle = (amount % 2) ? (amount / 2 + 1) : (amount / 2);
 
-    int temp = 0;
-
-    for (int i = 0; i < amount; i++) {
+    for (int i = 0; i < 9; i++) {
         if (pixels[i].num == lights[middle].num) {
             median = pixels[i].color;
-            temp = i;
         }
     }
-
-    //qDebug() << "median num " << lights[middle].num << "   " << "pixel num" << pixels[temp].num;
 
     return median;
 }
